972261a67326a95e10dbf1081947f7e0e4b9b47c
Ramkishore Saravanan <ramkishore07s@gmail.com>
2019-03-06 19:13:27 +0530

    output to video

--- src/tracking_dp.py
+++ src/tracking_dp.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import logging
 
 def sub(s, I):
     if len(I) > 0:
@@ -102,19 +102,59 @@ def tracking_dp(ndct, c_en, c_ex, c_ij, betta,
 
     return res, min_cs
 
-
 if __name__ == "__main__":
-    import cv2, numpy as np, os, pickle
+    import cv2, numpy as np, pickle
+    import os, shutil
+    from importlib import reload
+    import grapher
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logging.info("Loading pedestrian detections cache")
 
     with open('pedestrian_detections.pkl', 'rb') as f:
         dct = pickle.load(f)
 
-    from importlib import reload
-    import grapher
-
     reload(grapher)
     ndct = grapher.graphMaker(dct);
 
+    logging.info("Tracking objects in the video")
     res, min_cs = tracking_dp(ndct, 10., 10., 0, 0.2, 18, np.inf, False)
+    logging.info("Tracking complete")
 
-    print(res['x'][-10:])
+    def drawRect(img, x, y, w, h):
+        x1, y1 = int(x), int(y)
+        x2, y2 = int(x + w), int(y + h)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        return img
+
+
+    logging.info("Drawing bounding boxes on images")
+    shutil.copytree('seq03-img-left/', 'results/')
+    images_folder = 'results/'
+    images = sorted(os.listdir(images_folder))
+
+    images = [cv2.imread(os.path.join(images_folder, image)) for image in sorted(images)]
+
+    for i in range(len(res['x'])):
+        frame = res['fr'][i] - 1
+        images[frame] = drawRect(images[frame], res['x'][i], res['y'][i], res['w'][i], res['h'][i])
+
+    height, width, channels = images[0].shape
+
+    logging.info("Writing results to tracking.mp4")
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Be sure to use lower case
+    out = cv2.VideoWriter('tracking.mp4', fourcc, 20.0, (width, height))
+
+    for image in images:
+        out.write(image)
+
+    out.release()
+
+    logging.info("Deleting temporary folders")
+    shutil.rmtree('results/')
+
+
+
+
+
+
